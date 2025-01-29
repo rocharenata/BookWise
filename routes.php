81fba7a115a09a9ca458fdf9087a7afd2d6ba47e
Renata Rocha <renatarocha95@hotmail.com>
2025-01-29 17:13:32 -0300

    Erro resposta 404 no arquivo routes.php

---
+++ routes.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+$controller = 'index';
+
+if (isset($_SERVER['PATH_INFO'])) {
+    $controller = str_replace('/', '', $_SERVER['PATH_INFO']);
+}
+
+ 
+if (!file_exists("controllers/{$controller}.controller.php")) {
+    http_response_code(404); // Define o código de resposta HTTP para 404
+    echo "Página não existe";
+    die(); // Use exit() em vez de die() para garantir que nenhum outro código seja executado
+}
+
+require "controllers/{$controller}.controller.php";
+ 
