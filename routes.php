03fb180c6898ea32bcd392e4e161dade369e2290
Renata Rocha <renatarocha95@hotmail.com>
2025-02-03 16:11:13 -0300

    Alterado require_once, estilizado a pagina 404 e ajustado rotas

--- routes.php
+++ routes.php
@@ -1,5 +1,7 @@
 ﻿<?php
 
+require_once 'functions.php';
+
 function carregarController($controller) {
     require "controllers/{$controller}.controller.php";
 }
@@ -9,11 +11,7 @@ $controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
 if (!$controller) $controller = 'index';
 
 if (!file_exists("controllers/{$controller}.controller.php")) {
-    if (!headers_sent()) {
-        http_response_code(404);
-    }
-    echo "Página não encontrada";
-    die();
+    abort(404);
 }
 
 require "controllers/{$controller}.controller.php";
---
+++ views/404.view.php
@@ -0,0 +1,1 @@
+﻿<h1 class="text-lg font-bold mt-2"> Página nao encontrada</h1>
