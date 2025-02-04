5839b3866667ed86af2ad1f00f73bed15d979504
Renata Rocha <renatarocha95@hotmail.com>
2025-02-04 10:00:05 -0300

    corrigindo falha do desing da aplicacao ao mexer em routes

--- routes.php
+++ routes.php
@@ -1,18 +1,23 @@
 ﻿<?php
 
-require_once 'functions.php';
+$controller = 'index';
 
-function carregarController($controller) {
-    require "controllers/{$controller}.controller.php";
+if (isset($_SERVER['PATH_INFO'])) {
+    $controller = str_replace('/', '', $_SERVER['PATH_INFO']);
 }
 
-$controller = str_replace('/', '', parse_url($_SERVER['REQUEST_URI'])['path']);
-
-if (!$controller) $controller = 'index';
+if (isset($_SERVER['REQUEST_URI'])) {
+    $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+} else {
+    $uri = '/';
+}
 
 if (!file_exists("controllers/{$controller}.controller.php")) {
-    abort(404);
+    http_response_code(404);
+    echo "Página não existe";
+    die();
 }
 
 require "controllers/{$controller}.controller.php";
+
 ?>
