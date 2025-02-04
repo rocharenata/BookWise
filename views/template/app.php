7da4d70293b62d905ed61a65b55f92ef36cfe383
Renata Rocha <renatarocha95@hotmail.com>
2025-02-04 11:16:34 -0300

    retirando arquivos .php da url

--- views/template/app.php
+++ views/template/app.php
@@ -1,7 +1,4 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-
-<head>
+﻿<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Book Wise</title>
@@ -14,16 +11,16 @@
             <div class="font-bold text-xl tracking-wide">Book Wise</div>
             <ul class="flex space-x-4 font-bold">
                 <li><a href="/" class="text-lime-500">Explorar</a></li>
-                <li><a href="/meus-livros.php" class="font-semibold hover:underline">Meus Livros</a></li>
+                <li><a href="/meus-livros" class="font-semibold hover:underline">Meus Livros</a></li>
             </ul>
             <ul>
-                <li><a href="/login.php" class="hover:underline">Fazer login</a></li>
+                <li><a href="/login" class="hover:underline">Fazer login</a></li>
             </ul>
         </nav>
     </header>
 
     <main class="mx-auto max-w-screen-lg space-y-6 mt-0 pt-0">
-        <?php require "views/{$view}.view.php"; // Importa a view atual ?>
+    <?php require "views/{$view}.view.php"; // Importa a view atual ?>
     </main>
 </body>
 
